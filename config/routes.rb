5b80cabe4aa7b29a5c47e7e85879023f1d186c03
Marco Martins <marco_martins909@hotmail.co.uk>
2020-07-14 14:08:24 +0100

    created contact form page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'sectors', to: 'pages#sectors'
+  get 'contact', to: 'pages#contact'
   resources :jobs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
