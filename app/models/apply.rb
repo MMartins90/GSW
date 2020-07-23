8956bfe61471911e9a14b0897e800b05774b8ae6
chi tsang <cht.chihungtsang@gmail.com>
2020-07-23 17:45:47 +0100

    setup job application email

--- app/models/apply.rb
+++ app/models/apply.rb
@@ -1,2 +1,17 @@
-class Apply < ApplicationRecord
+class Apply < MailForm::Base
+  attribute :full_name,      :validate => true
+  attribute :location, :validate => true
+  attribute :cover_letter,      :attachment => true
+  attribute :cv,      :attachment => true
+  attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      :subject => "Job application - #{full_name}",
+      :to => "bradpfm@gmail.com",
+      :from => %("#{full_name}" <#{email}>)
+    }
+  end
 end
